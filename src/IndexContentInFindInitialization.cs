ef052a4dd75a4dd62a8690d8c43560da5088cdb4
Mattias Olsson <mattias@geta.no>
2019-01-03 11:21:42 +0100

    Fixes issue #3: Embed language file as resource.

---
+++ src/IndexContentInFindInitialization.cs
@@ -0,0 +1,51 @@
+﻿using System.Linq;
+using System.Reflection;
+using EPiServer.Framework;
+using EPiServer.Framework.Initialization;
+using EPiServer.Framework.Localization;
+using EPiServer.Framework.Localization.XmlResources;
+
+namespace Geta.Epi.IndexContentInFind
+{
+    [ModuleDependency(typeof(EPiServer.Web.InitializationModule))]
+    [InitializableModule]
+    public class IndexContentInFindInitialization : IInitializableModule
+    {
+        private const string LocalizationProviderName = "Geta.Epi.IndexContentInFind.EmbeddedLangFiles";
+        private static bool _initialized;
+        private static readonly object _lock = new object();
+
+        public void Initialize(InitializationEngine context)
+        {
+            lock (_lock)
+            {
+                if (_initialized)
+                {
+                    return;
+                }
+
+                AddLocalizationProvider(context.Locate.Advanced.GetInstance<LocalizationService>());
+                _initialized = true;
+            }
+        }
+
+        public void Uninitialize(InitializationEngine context)
+        {
+        }
+
+        private void AddLocalizationProvider(LocalizationService localizationService)
+        {
+            var providerBasedLocalizationService = localizationService as ProviderBasedLocalizationService;
+
+            if (providerBasedLocalizationService == null || providerBasedLocalizationService.ProviderList.Any(p => p.Name.Equals(LocalizationProviderName)))
+            {
+                return;
+            }
+
+            var localizationProviderInitializer = new EmbeddedXmlLocalizationProviderInitializer();
+            var localizationProvider = localizationProviderInitializer.GetInitializedProvider(LocalizationProviderName, Assembly.GetExecutingAssembly());
+
+            providerBasedLocalizationService.AddProvider(localizationProvider);
+        }
+    }
+}
--- src/Rest/IndexContentStore.cs
+++ src/Rest/IndexContentStore.cs
@@ -1,9 +1,7 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.Net;
 using System.Web.Mvc;
 using EPiServer.Core;
-using EPiServer.Find.Cms;
 using EPiServer.Shell.Services.Rest;
 
 namespace Geta.Epi.IndexContentInFind.Rest
@@ -11,38 +9,30 @@ namespace Geta.Epi.IndexContentInFind.Rest
     [RestStore("indexcontentstore")]
     public class IndexContentStore : RestControllerBase
     {
-        protected readonly IIndexService IndexService;
+        private readonly IIndexService _indexService;
 
         public IndexContentStore(IIndexService indexService)
         {
-            if (indexService == null) throw new ArgumentNullException("indexService");
-            IndexService = indexService;
+            _indexService = indexService ?? throw new ArgumentNullException(nameof(indexService));
         }
 
         [HttpPost]
-        public virtual ActionResult Post(IndexInFindData data)
+        public virtual ActionResult Post(IndexInFindRequest data)
         {
             if (data == null || ContentReference.IsNullOrEmpty(data.ContentLink))
             {
-                return new HttpStatusCodeResult(HttpStatusCode.InternalServerError, "ContentLink cannot be null.");
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest, "ContentLink cannot be null.");
             }
 
-            IEnumerable<ContentIndexingResult> indexingResults;
-
-            if (data.IncludeDescendants)
-            {
-                indexingResults = IndexService.IndexFrom(data.ContentLink, data.Force);
-            }
-            else
-            {
-                indexingResults = IndexService.Index(data.ContentLink, data.Force);
-            }
+            var indexingResults = data.IncludeDescendants
+                ? _indexService.IndexFrom(data.ContentLink, data.Force)
+                : _indexService.Index(data.ContentLink, data.Force);
 
             return Rest(indexingResults);
         }
     }
 
-    public class IndexInFindData
+    public class IndexInFindRequest
     {
         public ContentReference ContentLink { get; set; }
         public bool IncludeDescendants { get; set; }
